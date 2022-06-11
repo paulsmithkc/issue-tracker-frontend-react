69dc650c61e3089517d536fa9d6b5f1a1a80cb39
Paul Smith <paulsmithkc@gmail.com>
2022-06-11 12:47:01 -0500

    changing navbar items when the user logs in

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,38 +8,46 @@ function Header({ onLogout }) {
   return (
     <header id="Header">
       <nav className="navbar navbar-dark navbar-expand bg-dark">
-        <span className="navbar-brand mx-2">Issue Tracker</span>
+        <NavLink className="navbar-brand mx-2" to="/">
+          Issue Tracker
+        </NavLink>
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink className="nav-link" to="/">
               Home
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/project">
-              Projects
-            </NavLink>
-          </li>
+          {auth && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/project">
+                Projects
+              </NavLink>
+            </li>
+          )}
         </ul>
-        {auth && (
-          <span className="navbar-text ms-auto me-2">
-            {auth.email || auth.userId}
-          </span>
-        )}
-        {auth && (
-          <ul className="navbar-nav">
+        <ul className="navbar-nav ms-auto">
+          {auth && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/user/me">
+                {auth.email || auth.userId}
+              </NavLink>
+            </li>
+          )}
+          {auth && (
             <li className="nav-item">
               <a className="nav-link" onClick={onLogout} href="/login">
                 Logout
               </a>
             </li>
-          </ul>
-        )}
+          )}
+          {!auth && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+          )}
+        </ul>
       </nav>
     </header>
   );
