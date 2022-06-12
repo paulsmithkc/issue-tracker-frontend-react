3dfb902e56c5d6ac6b1277be638384f1d5ee9acf
Paul Smith <paulsmithkc@gmail.com>
2022-06-12 10:27:02 -0500

    installing basic service worker

--- src/index.js
+++ src/index.js
@@ -17,3 +17,21 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// check if browser supports service workers
+if (navigator.serviceWorker) {
+  // wait for the page to load
+  window.addEventListener('load', () => {
+    if (process.env.NODE_ENV === 'production') {
+      // register service worker in prod environment
+      navigator.serviceWorker.register(
+        `${process.env.PUBLIC_URL}/service-worker.js`
+      );
+    } else {
+      // unregister service worker in dev environment
+      navigator.serviceWorker.ready
+        .then((registration) => registration.unregister())
+        .catch((error) => console.error(error.message));
+    }
+  });
+}
