7248ef5a2a72ad4e69a00a99c0b6f680b0bf683c
Paul Smith <paulsmithkc@gmail.com>
2022-06-12 10:14:23 -0500

    updated logo and manifest

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ function Footer() {
   return (
     <footer id="Footer" className="bg-dark text-white text-center p-2">
       &copy; 2022 Paul Smith
+      <div style={{ fontSize: '0.8em' }}>
+        <a href="https://www.flaticon.com/free-icons/bug" title="bug icons">
+          Bug icons created by Freepik - Flaticon
+        </a>
+      </div>
     </footer>
   );
 }
