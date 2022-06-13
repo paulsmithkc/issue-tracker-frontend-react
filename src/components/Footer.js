d122585a02de8c30192702b728428e82cda25765
Paul Smith <prsmith@ranken.edu>
2022-06-13 13:07:35 -0500

    footer links open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,12 @@ function Footer() {
     <footer id="Footer" className="bg-dark text-white text-center p-2">
       &copy; 2022 Paul Smith
       <div style={{ fontSize: '0.8em' }}>
-        <a href="https://www.flaticon.com/free-icons/bug" title="bug icons">
+        <a
+          href="https://www.flaticon.com/free-icons/bug"
+          title="bug icons"
+          target="_blank"
+          rel="noreferrer"
+        >
           Bug icons created by Freepik - Flaticon
         </a>
       </div>
